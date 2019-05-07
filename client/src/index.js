8e3a95aaffe68c53d02a72209b4d9789d6d76ec7
MAllen07 <mrsmallen07@gmail.com>
2019-05-07 19:01:49 -0400

    moved everything in their own small components

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Assets/App.css";
+import App from "./App";
 
-import App from './App';
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
