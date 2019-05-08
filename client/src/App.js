54e0a54fa24a23febd7c4097ab5a607e1877073b
MAllen07 <mrsmallen07@gmail.com>
2019-05-08 11:29:38 -0400

    created button compoenent for the registration form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Header from "./Components /Header/Header";
 import RegistrationForm from "./Components /Registration/RegistrationForm";
+<<<<<<< HEAD
+=======
+import Login from "./Components /Login/Login";
+import RegButton from "./Components /Registration/RegButton";
+
+>>>>>>> created button compoenent for the registration form
 
 class App extends React.Component {
   render() {
     return (
       <div className="">
         <Header />
+<<<<<<< HEAD
         <RegistrationForm />
+=======
+        <Login />
+      
+    
+>>>>>>> created button compoenent for the registration form
       </div>
     );
   }
