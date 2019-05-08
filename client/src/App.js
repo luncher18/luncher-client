2dba4f2fa9a7b746972466fbf70304dcb65b001e
lrod <rleslie1015@gmail.com>
2019-05-08 16:53:46 -0700

    added nav

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,32 @@ import RegistrationForm from "./Components /Registration/RegistrationForm";
 import Login from "./Components /Login/Login";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import PrivateComponent from "./Components /PrivateComponent"
+
+import PrivateRoute from './Components /PrivateRoute';
+
 function AppRouter() {
   return (
     <Router>
       <div>
-        <Route exact path="/" exact component={Login} />
-        <Route path="/RegistrationForm/" component={RegistrationForm} />
-        <Route path="/Header/" component={Header} />
-      </div>
-    </Router>
+        <h1>
+            Welcome To Luncher
+          </h1>
+          <nav>
+            <a>
+              <Link to="/login">Login </Link>
+            </a>
+            <a>
+            <Link to="/protected">Protected Page</Link>
+            </a>
+          </nav>
+          <Route path="/login" component={Login} />
+          <PrivateRoute exact path="/protected" component={PrivateComponent} />
+          <Route exact path="/" exact component={Login} />
+          <Route path="/RegistrationForm/" component={RegistrationForm} />
+          <Route path="/Header/" component={Header} />
+        </div>
+    </Router>             
   );
 }
 
